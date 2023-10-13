2168cc2375deb981d0e8b28fce3db52c23e8d170
Oleg Zharov <levineye@gmail.com>
2023-10-13 23:33:59 +0300

    feat: добавить метод апи для сохранения выбранного купона

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -13,12 +13,49 @@ class Api {
         method: HttpMethods.get,
         headers: {
           Accept: 'application/json',
-          ['Content-Type']: 'application/json',
+          'Content-Type': 'application/json',
         },
       }
     );
 
     const data: TApiResponce<ICertificate[]> = await res.json();
+
+    return data;
+  }
+
+  public async saveCertificate({
+    id,
+    tablename,
+    primarykey,
+    price,
+    summa,
+    clientname,
+    phone,
+    email,
+  }: {
+    id: number;
+    tablename: string;
+    primarykey: string;
+    price: string;
+    summa: string;
+    clientname: string;
+    phone: string;
+    email: string;
+  }): Promise<TApiResponce<{ certNumber: string }>> {
+    const res: Response = await fetch(
+      `${this.url}?MethodName=OSSale&ApiKey=${
+        config.apiKey
+      }&Id=${id}&TableName=${tablename}&PrimaryKey=${primarykey}&Price=${price}&Summa=${summa}&ClientName=${clientname}&Phone=${phone}&Email=${email}&PaymentTypeId=${2}&UseDelivery=${0}`,
+      {
+        method: HttpMethods.post,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    const data: TApiResponce<{ certNumber: string }> = await res.json();
 
     return data;
   }
